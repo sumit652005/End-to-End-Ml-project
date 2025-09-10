502651f7daee637d4b4f275f86fe05c20cd3b73b
Sumit Das <sumitdas7864865@gmail.com>
2025-09-11 00:28:58 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    HYPEN_E_DOT = '-e .'
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+setup(
+    name="diabetes_prediction",
+    version="0.0.1",
+    author="Sumit Das",
+    author_email="sd2728181@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
